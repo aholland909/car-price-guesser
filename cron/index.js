c8420a7a2b08a0d11444be9b2ff9cb4e519976e4
Andrew Holland <andrewholland@C02F1NQTMD6M.local>
2022-01-19 11:51:23 +0000

    added arg inputs into cron

--- cron/index.js
+++ cron/index.js
@@ -1,13 +1,21 @@
-//upload data
+/*
+* Upload data
+* Options
+--stop  -> stop at duplicates 
+--sleep -> sleep time between cars
+*/
 
 const axios = require("axios");
 const cc = require("./requests.js");
 const car = require("../db/cars");
+const { getArgs } = require("../utils/getArgs");
 
 const sleep = (waitTimeInMs) =>
   new Promise((resolve) => setTimeout(resolve, waitTimeInMs));
 
-async function getCars() {
+const args = getArgs();
+
+const getCars = async () => {
   try {
     let page = 0;
     let getData = true;
@@ -27,17 +35,19 @@ async function getCars() {
         let findCar = await car.findCar(currentCar.auctionId);
         if (findCar) {
           console.log(`${currentCar.auctionId} is already in database`);
+          if (args.stop ? args.stop : false) getData = false;
         } else {
           //add to database
           car.uploadCar(currentCar);
           console.log(`Added ${currentCar.auctionId}`);
         }
+        await sleep(args.sleep ? args.sleep : 0);
       }
       page++;
     }
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 getCars();
